89a6c784c9aebfcdb5407b1aead1c4591e0d2577
Cássio Giehl <cassiogiehl@cassios-mbp.dsi.net.br>
2023-10-27 09:58:30 -0300

    fix query

--- dbt/spacex_transform/models/launches_cost.sql
+++ dbt/spacex_transform/models/launches_cost.sql
@@ -6,17 +6,11 @@ select
     l.id, 
     l.success,
     l.failures,
-    l.date_local,
-    l.rocket,
-    r.cost_per_launch
+    sum(r.cost_per_launch) as cost_per_launch
 from {{ ref('stg_launches') }} l
 left join {{ ref('stg_rockets') }} r
 on l.rocket = r.id
-
--- monthly_launch_cost as (
---     select 
---         l.date_local,
---         sum(r.cost_per_launch)
---     from launches_cost
---     group by l.date_local
--- )
+group by 
+    l.id, 
+    l.success,
+    l.failures
